fc69082dc0d86e5d86398ca4e902597998bf87f5
WebTechDUDE <takenolove13579@gmail.com>
2022-07-01 16:13:36 +0600

    nothing

---
+++ script.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector('.fa-bars');
+menu.style.color = "white";
+menu.onclick = () => {
+   menu.classList.toggle('fa-times');
+};
+
+
+
+
+
+
+
+// pop-overs
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+// tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+
+// Owl Carousel
